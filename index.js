7b242da783c9344791cfb8cc18525a4ec2ebb5af
Aaron <ahcollegebound@gmail.com>
2017-12-17 23:27:49 -0500

    Goal layout and audio updates.

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-var tday = new Array("Sun","Mon","Tues","Wed","Thur","Fri","Sat");
-var tmonth = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+var selectmp3 = new Audio();
+selectmp3.src = "Sounds/Final%20Fantasy%20VII%20Sound%20Effects%20-%20Cursor%20Move.mp3";
+selectmp3.preload = 'auto';
 
-function GetClock(){
-    var d=new Date();
-    var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
-    var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
-    
-    if(nhour==0){ap=" AM";nhour=12;}
-    else if(nhour<12){ap=" AM";}
-    else if(nhour==12){ap=" PM";}
-    else if(nhour>12){ap=" PM";nhour-=12;}
-    
-    if(nmin<=9) nmin="0"+nmin;
-     document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+" "+nhour+":"+nmin+ap+"";
+var backmp3 = new Audio();
+backmp3.src = "Sounds/Final%20Fantasy%20VII%20Sound%20Effects%20-%20Cursor%20Back.mp3";
+backmp3.preload ='auto';
+
+var tday = new Array("Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat");
+var tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+function GetClock() {
+    var d = new Date();
+    var nday = d.getDay(),
+        nmonth = d.getMonth(),
+        ndate = d.getDate();
+    var nhour = d.getHours(),
+        nmin = d.getMinutes(),
+        nsec = d.getSeconds(),
+        ap;
+
+    if (nhour == 0) {
+        ap = " AM";
+        nhour = 12;
+    } else if (nhour < 12) {
+        ap = " AM";
+    } else if (nhour == 12) {
+        ap = " PM";
+    } else if (nhour > 12) {
+        ap = " PM";
+        nhour -= 12;
+    }
+
+    if (nmin <= 9) nmin = "0" + nmin;
+    document.getElementById('clockbox').innerHTML = "" + tday[nday] + ", " + tmonth[nmonth] + " " + ndate + " " + nhour + ":" + nmin + ap + "";
 }
 
-window.onload=function(){
+window.onload = function () {
     GetClock();
-    setInterval(GetClock,1000);
-    
+    setInterval(GetClock, 1000);
+
     var statFile = "stats.txt"
-    
+
     var file = new File(statFile);
-    
+
     var statTable = document.getElementById("stats");
-    
-    for(var x = 0; x < 3; x++)
-        { 
-        for(var y = 0; y < 2; y++) {
+
+    for (var x = 0; x < 3; x++) {
+        for (var y = 0; y < 2; y++) {
             statTable.rows[x].cells[y].innerHTML = file.readln();
         }
     }
@@ -35,14 +54,21 @@ window.onload=function(){
     alert("Load Succesful");
 }
 
-function openConsole(consoleName){
-    document.getElementById("main_console").style.display = "none";
+function openConsole(consoleName) {
+    selectmp3.play();
     
+    document.getElementById("main_console").style.display = "none";
+
     document.getElementById(consoleName).style.display = "block";
+    
+    
 }
 
-function goBack(consoleName){
+function goBack(consoleName) {
+    backmp3.play();
     document.getElementById("main_console").style.display = "block";
-    
+
     document.getElementById(consoleName).style.display = "none";
+    
+    
 }
